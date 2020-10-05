7298c057105400c6f2f06b627c73691da11d99ee
ngrgur <ngrgur@mds.rs>
2020-10-05 14:30:16 +0200

    Score: 40/40 minimum-swaps-2

--- minimum-swaps-2.py
+++ minimum-swaps-2.py
@@ -6,14 +6,28 @@ import sys
 
 # Complete the minimumSwaps function below.
 def minimumSwaps(n, arr):
-    num_of_swaps = 0
-    for i in list(range(n-1)):
-        if arr[i] != i+1:
-            index_swap = arr.index(i+1)
-            arr[i], arr[index_swap] = arr[index_swap], arr[i]
+    # num_of_swaps = 0
+    # for i in list(range(n-1)):
+    #     if arr[i] != i+1:
+    #         index_swap = arr.index(i+1)
+    #         arr[i], arr[index_swap] = arr[index_swap], arr[i]
+    #
+    #         num_of_swaps += 1
+    # return num_of_swaps
 
-            num_of_swaps += 1
-    return num_of_swaps
+    num_of_swap = 0
+    index_dict = {value: index for index, value in enumerate(arr)}
+    for i,v in enumerate(arr):
+        correct_value = i+1
+        if v != correct_value:
+            index_to_swap = index_dict[correct_value]
+            arr[index_to_swap], arr[i] = arr[i], arr[index_to_swap]
+            index_dict[v] = index_to_swap
+            index_dict[correct_value] = i
+            num_of_swap +=1
+
+    return num_of_swap
+
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
